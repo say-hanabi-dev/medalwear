6ee541e28cabd5c362c8f62503cd779a45b7a2ee
默一 <1254846416@qq.com>
2021-12-15 10:10:06 +0800

    修复勋章列表显示

--- medalwear.class.php
+++ medalwear.class.php
@@ -16,6 +16,24 @@ class plugin_medalwear_home extends plugin_medalwear
 {
     function medal_nav_extra()
     {
-        return '<li><a href="home.php?mod=spacecp&ac=plugin&id=medalwear:memcp">勋章佩戴</a></li>';
+        global $_G;
+        $memberMedals = DB::fetch_all('SELECT * FROM '.DB::table('common_member_medal').' where uid='.$_G['uid']);
+        $memberMedalIds = [];
+        foreach ($memberMedals as $memberMedal)
+            $memberMedalIds[] = $memberMedal['medalid'];
+        $memberMedalIdsStr = implode(',', $memberMedalIds);
+        $jsvar = "memberMedalIds = [".$memberMedalIdsStr."];";
+        $jsfunc = <<<EOF
+            function changeMedalText(item, index){
+                medal = document.getElementById("medal_"+item);
+                li = medal.parentElement;
+                p = li.lastElementChild;
+                p.innerHTML="已拥有";
+            }
+            
+            memberMedalIds.forEach(changeMedalText);
+EOF;
+        $jscode = '<script>'.$jsvar.$jsfunc."</script>";
+        return '<li><a href="home.php?mod=spacecp&ac=plugin&id=medalwear:memcp">勋章佩戴</a></li>'.$jscode;
     }
 }
